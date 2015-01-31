f434797157ae0f81728df8c46ff5ecafaa0871e1
Andrey Sitnik <andrey@sitnik.ru>
2015-01-31 16:19:55 +0300

    Add base variables support for declaration value

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-module.exports = function (css) { };
+var definition = function (variables, node) {
+    var name = node.prop.slice(1);
+    variables[name] = node.value;
+    node.removeSelf();
+};
+
+var declValue = function (variables, node) {
+    node.value = node.value.replace(/\$[\w\d-]+/, function (str) {
+        var name = str.slice(1);
+        if ( variables[name] ) {
+            return variables[name];
+        } else {
+            throw node.error('Undefined variable ' + str);
+        }
+    });
+};
+
+module.exports = function (opts) {
+    if ( typeof(opts) == 'undefined' ) opts = { };
+
+    var variables = { };
+
+    return function (css) {
+        css.eachInside(function (node) {
+
+            if ( node.type == 'decl' ) {
+                if ( node.prop[0] == '$' ) {
+                    definition(variables, node);
+                } else {
+
+                    if ( node.value.indexOf('$') != -1 ) {
+                        declValue(variables, node);
+                    }
+                }
+
+            }
+
+        });
+    };
+};
+
+module.exports.postcss = function (css) {
+    module.exports()(css);
+};
--- test/vars.js
+++ test/vars.js
@@ -3,4 +3,26 @@ var expect  = require('chai').expect;
 
 var vars = require('../');
 
-describe('postcss-simple-vars', function () { });
+var test = function (input, output) {
+    expect(postcss(vars).process(input).css).to.eql(output);
+};
+
+describe('postcss-simple-vars', function () {
+
+    it('replaces variables in values', function () {
+        test('$size: 10px;\n' + 'a { width: $size; height: $size; }',
+             'a { width: 10px; height: 10px; }');
+    });
+
+    it('overrides variables', function () {
+        test('$var: 1; a{ one: $var } b{ $var: 2; two: $var } c{ two: $var }',
+             'a{ one: 1 } b{ two: 2 } c{ two: 2 }');
+    });
+
+    it('throws an error on unknown variable', function () {
+        expect(function () {
+            test('a { width: $size }');
+        }).to.throw('<css input>:1:5: Undefined variable $size');
+    });
+
+});
